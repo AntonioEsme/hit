0fd4deaa4afd91ac31542829b36457b5deaccb0b
esmeantonio <esmeantonio1125@gmail.com>
2020-12-04 22:42:21 -0600

    github

---
+++ css/tablas.php
@@ -0,0 +1,15 @@
+table {
+     margin:0 auto;
+   width: 50%;
+      border: 1px solid #000;   
+}
+   
+th, td {
+ padding: 10px 20px 10px 20px;
+      background-color:#FCFAE1;
+    
+}
+#encabezado{
+    color:black;
+    background-color:#BD8D46;
+}
